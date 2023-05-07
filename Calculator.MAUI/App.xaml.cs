736c66144f9915f511031e8319645a7acafe585b
Badhan Sen <badhan.cse@gmail.com>
2023-05-07 23:34:39 +0600

    Initial commit

---
+++ Calculator.MAUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Calculator.MAUI;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ Calculator.MAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Calculator.MAUI;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
